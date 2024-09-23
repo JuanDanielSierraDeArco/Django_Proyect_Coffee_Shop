1fb72cd2028254d519b03a62e62fb9e727ee7298
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-09-23 02:21:53 -0500

    agregando admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'description', 'price', 'available', 'photo']
+
+admin.site.register(Product, ProductAdmin)
